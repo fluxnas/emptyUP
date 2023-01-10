f73205c09f51b9b0dc5ab0616085347409532769
Arseniia <arseniyadamaksina@gmail.com>
2023-01-10 14:24:09 +0100

    Adding a map

---
+++ client/src/maps/leaflet-map.jsx
@@ -0,0 +1,27 @@
+import markerIcon from "leaflet/dist/images/marker-icon.png"
+import { Marker, Popup, useMap } from 'react-leaflet';
+import useGeoLocation from '../hooks/geoLocationHook';
+import useUserDefaultLocation from '../hooks/userDefaultPositionHook';
+import { useEffect } from 'react';
+import L from 'leaflet';
+
+export const LeafletMap = () => {
+    const { position } = useGeoLocation();
+    const { userLocation } = useUserDefaultLocation(position);
+
+    const map = useMap();
+    
+    useEffect(() => {
+        map.setView(userLocation);
+    }, [userLocation]);
+
+    const userIcon = L.icon({
+        iconUrl: markerIcon,
+        iconSize: [22, 38],
+        iconAnchor: [11, 19],
+    });
+    return <div>
+        <Marker position={userLocation} icon={userIcon}>
+        </Marker>
+    </div>;
+}

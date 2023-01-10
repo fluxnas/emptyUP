0aaaf774780d54bb6a09663dcaddc46937b1b514
Arseniia <arseniyadamaksina@gmail.com>
2023-01-10 16:22:49 +0100

    Uploaded page

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,20 +1,19 @@
-
-import Navigation from "../components/Navigation"
-import UploadLogo from "../components/UploadLogo"
+import Navigation from "../components/Navigation";
+import UploadLogo from "../components/UploadLogo";
 import { LeafletContainer } from "../maps/leaflet-container";
 import { LeafletMap } from "../maps/leaflet-map";
 
-const HomePage =() => {
-	return(
-		<div className="Homepage">
-			<h1>HOMEPAGE</h1>
-			<Navigation />
+const HomePage = () => {
+  return (
+    <div className="Homepage">
+      <h1>HOMEPAGE</h1>
+      <Navigation />
       <LeafletContainer>
-      <LeafletMap />
-    </LeafletContainer>
-			<UploadLogo/>
-		</div>
-	)
-}
+        <LeafletMap />
+      </LeafletContainer>
+      <UploadLogo />
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;

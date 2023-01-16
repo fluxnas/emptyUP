542c468de57377aad6642f0be51bf6a129cdcb5f
jeunessesept <b.jiacinto@gmail.com>
2023-01-16 16:56:35 +0100

    last of 16/01

--- server/controllers/images.mjs
+++ server/controllers/images.mjs
@@ -1,57 +1,103 @@
 import { pool } from "../models/dbPool.mjs";
-import cloudinary from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 import { request } from "express";
+import path from 'path';
 import multer from "multer";
+import datauri from "datauri";
+
+
 
 const storage = multer.memoryStorage();
 const maxSize = 5000000;
 const upload = multer({ storage, limits: { fileSize: maxSize } });
 
-export const uploadImage = (req, res) => {
-  
-  upload.single("image")(req, res, (err) => {
-    
-      // Use `req.file` to access the file that was uploaded
-      const data = {
-        image: req.file.buffer,
-      };
-      console.log(req.file.buffer)
-      
-      const building_id = "3";
-      cloudinary.v2.uploader
-        .upload(req.file.originalname, {resource_type: "auto" })
-        .then((image) => {
-          pool
-            .query(
-              "insert into images (cloudinary_id, image_url, building_id ) values ($1, $2, $3) RETURNING * ",
-              [image.public.id, image.secure_url, building_id]
-            )
-            .then((result) => {
-              result = result.rows[0];
-              res.status(201).send({
-                status: "success",
-                data: {
-                  message: "Image Uploaded Successfully",
-                  cloudinary_id: result.cloudinary_id,
-                  image_url: result.image_url,
-                },
-              });
-            })
-            .catch((e) => {
-              res.status(500).send({
-                message: "failure",
-                e,
-              });
-            });
-        })
-        .catch((error) => {
-          console.log(error);
-          res.status(500).send({
-            message: "Upload failed",
-            error: error,
-          });
-        });
-    })
-  
+
+
+
+
+
+
+const dataUri = (req) => {
+  dUri.format(path.extname(req.file.originalname).toString(), req.file.buffer);
 }
-// find alternative to cloudinary ? 
+
+export const uploadImage = async (req, res) => {
+  console.log(req.file);
+
+
+  // const file = await dataUri(req).content;
+  // console.log(file);
+
+  // cloudinary.uploader.upload(file).then((fileUpload) => {
+  // console.log(fileUpload);
+  // pool
+  //   .query(
+  //     "insert into images (cloudinary_id, image_url, building_id ) values ($1, $2, $3) RETURNING * ",
+  //     [file.public.id, file.secure_url, building_id]
+  //   )
+  //   .then((result) => {
+  //     result = result.rows[0];
+  //     res.status(201).send({
+  //       status: "success",
+  //       data: {
+  //         message: "Image Uploaded Successfully",
+  //         cloudinary_id: result.cloudinary_id,
+  //         image_url: result.image_url,
+  //       },
+  //     });
+  //   })
+  //   .catch((e) => {
+  //     res.status(500).send({
+  //       message: "failure",
+  //       e,
+  //     });
+  //   });
+  // });
+};
+
+// export const uploadImage = async (req, res) => {
+//   // const image_u = await upload.single("file")
+//   upload.single("file", req, res, (err) => {
+//     const file = req.file;
+//     console.log(req.file);
+//     // console.log(image);
+//     // return image;
+
+//     const building_id = "3";
+//     cloudinary.uploader
+//       .upload(image)
+//       .then((file) => {
+//         console.log(file);
+//         // pool
+//         //   .query(
+//         //     "insert into images (cloudinary_id, image_url, building_id ) values ($1, $2, $3) RETURNING * ",
+//         //     [file.public.id, file.secure_url, building_id]
+//         //   )
+//         //   .then((result) => {
+//         //     result = result.rows[0];
+//         //     res.status(201).send({
+//         //       status: "success",
+//         //       data: {
+//         //         message: "Image Uploaded Successfully",
+//         //         cloudinary_id: result.cloudinary_id,
+//         //         image_url: result.image_url,
+//         //       },
+//         //     });
+//         //   })
+//         //   .catch((e) => {
+//         //     res.status(500).send({
+//         //       message: "failure",
+//         //       e,
+//         //     });
+//         //   });
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//         res.status(500).send({
+//           message: "Upload failed",
+//           error: error,
+//         });
+//       });
+//   });
+// };
+// find alternative to cloudinary ?
--- server/server.mjs
+++ server/server.mjs
@@ -1,56 +1,62 @@
 import express from "express";
-import bodyParser from "body-parser"
+import bodyParser from "body-parser";
+import fileUpload from "express-fileupload";
 
-import { getBuildings, getBuilding, addBuilding } from "./controllers/buildings.mjs";
-import { dbConnect } from "./models/dbConnect.mjs"
+
+import {
+  getBuildings,
+  getBuilding,
+  addBuilding,
+} from "./controllers/buildings.mjs";
+
+
+import { dbConnect } from "./models/dbConnect.mjs";
 import cookie from "cookie-parser";
-import dotenv from "dotenv"
-import {register, login } from "./controllers/users.mjs"
-import { uploadImage } from "./controllers/images.mjs";
+import dotenv from "dotenv";
+import { register, login } from "./controllers/users.mjs";
+import { uploadImage } from "./controllers/imagesUpload.mjs";
 import jwtAuthentification from "./middleware/verifyToken.mjs";
-import cloudinary from "cloudinary"
-import multer from "multer";
-
-dotenv.config()
-dbConnect()
-const storage = multer.memoryStorage();
-const server = express()
-server.use(multer({ storage: storage }).single('image'));
+import * as cloudinary from "cloudinary";
 
 
 
+dotenv.config();
+dbConnect();
+const server = express();
 
-server.use(express.json())
+
+server.use(express.json());
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(cookie(process.env.SECRET_JWT));
+server.use(fileUpload({
+    useTempFiles: true,
+    limits: {fileSize: 50 * 2024 * 1024}
+}))
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDI_NAME,
-    api_key: process.env.CLOUDI_APIKEY,
-    api_secret: process.env.CLOUDI_APISEC
-    
-})
+  cloud_name: 'dn5m5l5tm',
+  api_key: '521678454952891',
+  api_secret: 'F-pnU7X4E7fg0vY2q6L6NQ-E04k'
+});
+
 
 
 server.get("/", (req, res) => {
-    res.send("hello")
-})
+  res.send("hello");
+});
 
 // user related endpoints
-server.post("/api/user/register", register)
-server.post("/api/user/login", login)
+server.post("/api/user/register", register);
+server.post("/api/user/login", login);
 
 // buildings related endpoints
-server.post("/api/addbuilding", jwtAuthentification ,addBuilding )
-server.get("/api/building", getBuilding)
+server.post("/api/addbuilding", jwtAuthentification, addBuilding);
+server.get("/api/building", getBuilding);
 
 // images related endpoints
-server.post("/api/building/uploadimage", uploadImage)
-
-
-
+server.post("/api/building/uploadimage", uploadImage);
 
 server.listen(5500, () => {
-    console.log("app is runing");
-  });
+  console.log("app is runing");
+});

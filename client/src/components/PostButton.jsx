42a2dc29ac2eaaa4e1abdb0cfaadffa3c2d551b9
aurorevd <aurorevandamme@gmail.com>
2023-01-17 16:53:08 +0100

    page message

---
+++ client/src/components/PostButton.jsx
@@ -0,0 +1,6 @@
+const PostButton = ({onClick}) => { 
+    return (
+<button  onClick={onClick} className="bg-blue-800 text-white rounded-md px-3 py-1 text-base font-medium hover:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-blue-800" >Post
+          </button>
+          )}
+export default PostButton
--- client/src/components/ProfilePicture.jsx
+++ client/src/components/ProfilePicture.jsx
@@ -2,8 +2,9 @@ import { NavLink } from 'react-router-dom'
 import Pp from '../assets/Pp.png';
 const ProfilePicture = () => {
 	return (
-		<div className="hover:scale-125 px-5 ">
-					<img src= {Pp} alt="profile picture" className="  rounded-full  w-32 h-32 " />	
+		<div className=" px-5 ">
+					<img src= {Pp} alt="profile picture" className="    m-2 w-32 h-32 " />	
+
 
 		</div>	
 
